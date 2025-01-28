3a3a3b5881ba2527e54ee9143ce1b3279fbd7ae3
Dmytro Bozhkov <dmitrijbozhkov@gmail.com>
2025-01-28 09:31:34 +0100

    Fix Batchnorm Issue

--- models/quantization/perturb_parametrize.py
+++ models/quantization/perturb_parametrize.py
@@ -2,7 +2,9 @@ from torch import nn
 from torch.nn.utils.parametrize import register_parametrization
 from operator import attrgetter
 from models.quantization.utils import check_legal
+from torch.nn.modules.batchnorm import _NormBase
 import torch
+
 
 class Perturbation(nn.Module):
     """Parametrization to add perturbation noise to model weights"""
@@ -11,6 +13,7 @@ class Perturbation(nn.Module):
         super().__init__(*args, **kwargs)
         self.perturb_mean = perturb_mean
         self.perturb_variance = perturb_variance
+        self.batchnorm_layers = []
         self.is_perturb = False
 
 
@@ -38,13 +41,30 @@ class Perturbation(nn.Module):
         """
         self.is_perturb = is_perturb
 
+        for b_layer in self.batchnorm_layers:
+            b_layer.track_running_stats = not self.is_perturb
+
+
+    def set_batchnorm_layers(self, batchnorm_layers):
+        """
+        Sets batchnorm layers to turn off statistics tracking
+
+        :param batchnorm_layers: List of batchnorm layers
+        """
+        self.batchnorm_layers = batchnorm_layers
+
 
     @classmethod
     def prepare_model_weights(cls, model, perturb_mean: float, perturb_variance: float):
         parametrizaiton = cls(perturb_mean, perturb_variance)
 
+        batchnorm_modules = []
         for module_name, module in  model.named_modules():
             if module_name and check_legal(module):
                 register_parametrization(module, "weight", parametrizaiton)
+            if isinstance(module, _NormBase):
+                batchnorm_modules.append(module)
+
+        parametrizaiton.set_batchnorm_layers(batchnorm_modules)
 
         return parametrizaiton
