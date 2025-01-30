2ed3778a8a4432dd44a3ed702f1c925c08137699
Dmytro Bozhkov <dmitrijbozhkov@gmail.com>
2025-01-30 23:18:09 +0100

    Fix running script

--- scripts/cluster_scripts/resnet18_perturb_loss_01_experiment.sh
+++ scripts/cluster_scripts/resnet18_perturb_loss_01_experiment.sh
@@ -1,3 +1,3 @@
 ./scripts/cluster_scripts/cluster_startup.sh
 
-python main.py --config-name="resnet18_perturb_loss_01_experiment" hpo_config.database="sqlite:////project/NHWP25179/run_out/hpo/resnet18_perturb_loss_01_experiment.db" training_config.output="/project/NHWP25179/run_out/outputs"
+python main.py --config-name="resnet18_perturb_loss_01_experiment" hpo_config.database="sqlite:////bigwork/nhwpbozd/hpo/resnet18_perturb_loss_01_experiment.db" training_config.output="/bigwork/nhwpbozd/run_out/outputs"
