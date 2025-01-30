ce645eb80b662d471ac4db64f32dcff7ec4081cc
Dmytro Bozhkov <dmitrijbozhkov@gmail.com>
2025-01-30 23:09:55 +0100

    Fix script

--- scripts/cluster_scripts/resnet18_perturb_experiment.sh
+++ scripts/cluster_scripts/resnet18_perturb_experiment.sh
@@ -1,3 +1,3 @@
-./cluster_startup.sh
+./scripts/cluster_scripts/cluster_startup.sh
 
 python main.py --config-name="resnet18_perturb_experiment" hpo_config.database="sqlite:////project/NHWP25179/run_out/hpo/resnet18_perturb_experiment.db" training_config.output="/project/NHWP25179/run_out/outputs"
